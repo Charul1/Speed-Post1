4c1c56c426dc316e1b21e60ade79eba9f5dbef46
Charul <charulkachhara1@gmail.com>
2017-07-05 02:28:42 +0530

    changes in hindi xml files

--- app/src/main/java/com/example/rachiket/speed_post/GenerateQR_Hindi.java
+++ app/src/main/java/com/example/rachiket/speed_post/GenerateQR_Hindi.java
@@ -1,8 +1,41 @@
 package com.example.rachiket.speed_post;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by udaipur on 7/3/2017.
  */
 
-public class GenerateQR_Hindi {
+public class GenerateQR_Hindi extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.generating_qr_hindi);
+
+        Button printBut = (Button) findViewById(R.id.generate_code_hindi);
+        printBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), PrintQR_Hindi.class);
+                startActivity(i);
+            }
+        });
+
+        Button backBut = (Button) findViewById(R.id.back_from_generate_hindi);
+        backBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), Hindi.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+
 }
--- app/src/main/java/com/example/rachiket/speed_post/UseKioskHindi.java
+++ app/src/main/java/com/example/rachiket/speed_post/UseKioskHindi.java
@@ -1,8 +1,31 @@
 package com.example.rachiket.speed_post;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by udaipur on 7/3/2017.
  */
 
-public class UseKioskHindi {
+public class UseKioskHindi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.use_kiosk_hindi);
+
+        Button backButHindi = (Button)findViewById(R.id.back_use_kiosk_hindi);
+        backButHindi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(),Hindi.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
 }
